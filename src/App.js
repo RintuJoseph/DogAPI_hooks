a25be8ff0c9df3a40bf6d650fb1f5101e03c9185
RintuJoseph <rintu.joseph784@gmail.com>
2022-07-20 14:49:33 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
-import "./styles.css";
+import React, { useState, useEffect } from "react";
 
-export default function App() {
+function App() {
+  const [data, setData] = useState("");
+  useEffect(() => {
+    console.log("useEffect called!");
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((res) => res.json())
+      .then((data) => setData(data.message))
+      .catch((err) => console.log("Oh noes!", err));
+  }, []);
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <img src={data} alt="A random dog breed" />
     </div>
   );
 }
+
+export default App;
